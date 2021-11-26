099f9eaba7404e6b0d3e1d654c1c3d9f56de5e9e
SantiguzmaN <santiagoguzmanarango@gmail.com>
2021-11-25 21:16:46 -0500

    _app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
-import '../styles/globals.css'
+import React from 'react';
 import ExpensesProvider from '../context/expensesProvider';
 import GlobalStateProvider from '../context/globalStateProvider';
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
