651b2ffdb65ef74bfae4cbc7f8e1f64387ed771e
SantiguzmaN <santiagoguzmanarango@gmail.com>
2021-11-28 22:57:05 -0500

    recursividad

--- pages/expenses/expenses.jsx
+++ pages/expenses/expenses.jsx
@@ -1,29 +1,55 @@
 import React, { useEffect, useState } from 'react';
 import { listExpenses } from '../../actions/expensesActions';
-import Table from '../../components/dataTable/dataTable';
+import Table from '../../components/table/table';
 import i18n from '../../i18n';
 
 const Expenses = () => {
   const text = <h3>{i18n.t('loading_info')}</h3>;
   const [view, setView] = useState(text);
   const [info] = useState([]);
+  const [atributes] = useState([]);
 
-  useEffect(() => {
+            // data.forEach(element => {
+          //   console.log(element.date.key)
+          //   // const element = {
+          //   //   date: element.date,
+          //   //   etype: element.expense_type,
+          //   //   price: element.price,
+          //   //   supplier: element.supplier
+          //   // }
+          // })
+          // data.forEach(element => {
+          //   console.log('data', element.date.key)
+          //   const elementoo = {
+          //     date: element.date,
+          //     etype: element.expense_type,
+          //     price: element.price,
+          //     supplier: element.supplier
+          //   }
+          // })
+
+  const findExpenses = () => {
+    console.log('entro al find');
     listExpenses().then(data => {
-      if (data) {
+      if (data[0]) {
         data.forEach(data => {
-          info.push(data.attributes);
+          console.log(data);
+          atributes = (...atributes, data.attributes);
         });
+      } else {
+        findExpenses();
       }
     });
+  };
+
+  useEffect(() => {
+    findExpenses();
   }, []);
 
   useEffect(() => {
-    setTimeout(() => {
-      setView(<Table info={info} />);
-    }, 2000);
-  }, [info]);
 
+    setView(<Table info={info} />);
+  }, [info]);
 
   return (
     <div >
